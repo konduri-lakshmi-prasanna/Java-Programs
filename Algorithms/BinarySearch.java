07105ae7896fd8cb14e295ba98ca55b268c57afb
Pallavi Kathait <kathaitpallavi164@gmail.com>
2025-10-13 18:42:16 +0530

    Create BinarySearch.java
    This implementation takes a sorted array and finds the index of a specified element, or returns -1 if the element is not found.

---
+++ Algorithms/BinarySearch.java
@@ -0,0 +1,29 @@
+public class BinarySearch {
+    // Performs binary search on a sorted array
+    public static int binarySearch(int[] arr, int key) {
+        int left = 0, right = arr.length - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (arr[mid] == key) {
+                return mid; // key found
+            } else if (arr[mid] < key) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return -1; // key not found
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {2, 4, 7, 11, 15, 19, 23, 28};
+        int key = 15;
+        int result = binarySearch(arr, key);
+
+        if (result == -1) {
+            System.out.println(key + " not found in the array.");
+        } else {
+            System.out.println(key + " found at index: " + result);
+        }
+    }
+}
